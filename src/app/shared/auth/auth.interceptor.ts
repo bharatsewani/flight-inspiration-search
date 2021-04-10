aa1a4586be8510b526cb3d5185f3d63d3f052025
Bharat Sewani <bharatsewani1990@gmail.com>
2021-04-10 19:43:03 +0530

    Integrated getToken call.

--- src/app/shared/auth/auth.interceptor.ts
+++ src/app/shared/auth/auth.interceptor.ts
@@ -3,34 +3,29 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { from } from 'rxjs';
 import { AuthService } from './auth.service';
+import { ApiConstants } from '../../constants/api-constants';
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(public authService: AuthService){
+  constructor(public authService: AuthService) {
 
   }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    // req = req.clone({
-    //   setHeaders: {
-    //     'Content-Type' : 'application/json; charset=utf-8',
-    //     'Accept'       : 'application/json',
-    //     'Authorization': `Bearer testCode`,
-    //   },
-    // });
-
-    // return next.handle(req);
     return from(this.handleAccess(req, next));
   }
 
-  private async handleAccess(request: HttpRequest<any>, next: HttpHandler){
-    let token = await this.authService.getToken();
-    let  req = request.clone({
-      setHeaders: {
-        'Content-Type' : 'application/json; charset=utf-8',
-        'Accept'       : 'application/json',
-        'Authorization': `Bearer ${token}`,
-      },
-    });
+  private async handleAccess(request: HttpRequest<any>, next: HttpHandler) {
+    if (request.url !== `${ApiConstants.BASE_URL}${ApiConstants.GET_TOKEN}`) {
+      let token = await this.authService.getToken();
+      let req = request.clone({
+        setHeaders: {
+          'Authorization': `Bearer ${token}`,
+        }
+      });
 
-    return next.handle(req).toPromise();
+      return next.handle(req).toPromise();
+    } else {
+      return next.handle(request).toPromise();
+    }
+
   }
 }
